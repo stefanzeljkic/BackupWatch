1ad0208119d4ada4a493d780c953ea9882ba311e
stefanzeljkic <127971073+stefanzeljkic@users.noreply.github.com>
2024-08-15 15:12:19 +0200

    Update install_backupwatch.sh

--- install_backupwatch.sh
+++ install_backupwatch.sh
@@ -18,12 +18,12 @@ else
 fi
 
 # 4. Install the required libraries from requirements.txt if it exists
-#    and force the installation of specific versions of Flask, Werkzeug, and Flask-WTF
+#    and force the installation of specific versions of Flask, Werkzeug, Flask-WTF, and Jinja2
 if [ -f "requirements.txt" ]; then
-    pip3 install --user --force-reinstall Flask==2.0.1 Werkzeug==2.0.1 Flask-WTF==0.14.3 SQLAlchemy==1.4.15 requests==2.25.1
+    pip3 install --user --force-reinstall Flask==2.0.1 Werkzeug==2.0.1 Flask-WTF==0.14.3 Jinja2==3.0.1 SQLAlchemy==1.4.15 requests==2.25.1
 else
     echo "requirements.txt file not found, installing Flask and Flask-WTF manually..."
-    pip3 install --user Flask==2.0.1 Werkzeug==2.0.1 Flask-WTF==0.14.3
+    pip3 install --user Flask==2.0.1 Werkzeug==2.0.1 Flask-WTF==0.14.3 Jinja2==3.0.1
 fi
 
 # 5. Enable UFW (firewall)
